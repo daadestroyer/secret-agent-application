bb0540db539bf3a67753e3733055f00ce8d7184d
daadestroyer <nigamshubham2000@gmail.com>
2021-04-28 19:11:11 +0530

    secret-agent-application-version-1-created

--- secret-agent-application.sh
+++ secret-agent-application.sh
@@ -10,22 +10,41 @@
 echo "WELCOME TO SECRET AGENT APPLICATION"
 echo "########################"
 read -p "Enter agent name : " AGENT_NAME
-read -p "Enter agent favourite actor : " ACTOR
-read -p "Enter agent lucky number : " LUCKY_NUMBER
-read -p "Enter agent favourite dish : " DISH
-
 FIRST_CHARACTER_OF_NAME=$(echo -n $AGENT_NAME | cut -c 1)
-len=$(echo -n $ACTOR | wc -c)
-ACTOR_LAST_CHARACTER=$(echo -n $ACTOR | cut -c $len)
-NUM_OF_CHARACTER_IN_DISH=$(echo -n $DISH | wc -c)
-
-if [ $FIRST_CHARACTER_OF_NAME = "s" -a $ACTOR_LAST_CHARACTER = "r" -a $LUCKY_NUMBER -eq 7 -a $NUM_OF_CHARACTER_IN_DISH -ge 6 ]; then
-    echo "*********************************************"
-    echo "Hello Secret $AGENT_NAME"
-    echo "We have to kill atleast 10 terrorist in India"
-    echo "*********************************************"
-else
+if [ $FIRST_CHARACTER_OF_NAME != "s" ]; then
    echo "*********************************************"
    echo "Hello $AGENT_NAME thank you for information..."
    echo "*********************************************"
+   exit 1
 fi
+
+read -p "Enter agent favourite actor : " ACTOR
+len=$(echo -n $ACTOR | wc -c)
+ACTOR_LAST_CHARACTER=$(echo -n $ACTOR | cut -c $len)
+if [ $ACTOR_LAST_CHARACTER != "r" ]; then
+   echo "*********************************************"
+   echo "Hello $AGENT_NAME thank you for information..."
+   echo "*********************************************"
+   exit 1
+fi   
+
+read -p "Enter agent lucky number : " LUCKY_NUMBER
+if [ $LUCKY_NUMBER -ne 7 ]; then
+   echo "*********************************************"
+   echo "Hello $AGENT_NAME thank you for information..."
+   echo "*********************************************"
+   exit 1
+fi   
+
+read -p "Enter agent favourite dish : " DISH
+NUM_OF_CHARACTER_IN_DISH=$(echo -n $DISH | wc -c)
+if [ $NUM_OF_CHARACTER_IN_DISH -lt 6 ]; then
+   echo "*********************************************"
+   echo "Hello $AGENT_NAME thank you for information..."
+   echo "*********************************************"
+   exit 1
+fi 
+echo "*********************************************"
+echo "Hello Secret $AGENT_NAME"
+echo "We have to kill atleast 10 terrorist in India"
+echo "*********************************************"   
